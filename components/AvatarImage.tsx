804843c0d5e51cfa5946fe9f8621156a99056e81
Muhammad Angga Adytya <anggaanggaksp@gmail.com>
2023-12-08 13:42:46 +0700

    feat: update avatarImage

--- components/AvatarImage.tsx
+++ components/AvatarImage.tsx
@@ -1,5 +1,7 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 interface ImageProps {
   className?: string;
@@ -7,16 +9,27 @@ interface ImageProps {
 
 const AvatarImage = ({ className }: ImageProps) => {
   return (
-    <div className={`${className} rounded-full bg-neutral-600`}>
-      <Image
-        src="/heroImage.svg"
-        alt="logo"
-        width={900}
-        height={900}
-        className="object-cover w-full h-full rounded-full"
-        loading="eager"
-      />
-    </div>
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5, delay: 0.5 }}
+    >
+      <motion.div
+        initial={{ filter: "blur(20px)" }}
+        animate={{ filter: "blur(0px)" }}
+        transition={{ duration: 1, delay: 0.5 }}
+        className={`${className} rounded-full bg-neutral-600`}
+      >
+        <Image
+          src="/heroImage.svg"
+          alt="logo"
+          width={900}
+          height={900}
+          className="object-cover w-full h-full rounded-full"
+          loading="lazy"
+        />
+      </motion.div>
+    </motion.div>
   );
 };
 
