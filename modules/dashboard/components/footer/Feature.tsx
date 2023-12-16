fff489580451220d4aaf62a8e220e9c3c830fa9b
Muhammad Angga Adytya <anggaanggaksp@gmail.com>
2023-12-16 12:51:55 +0700

    update: SEO

--- modules/dashboard/components/footer/Feature.tsx
+++ modules/dashboard/components/footer/Feature.tsx
@@ -12,26 +12,26 @@ const Feature = () => {
         <div className="bg-neutral-700/50 w-8 h-8 rounded-full flex items-center justify-center">
           <PiGithubLogoLight className="group-hover:text-neutral-200" />
         </div>
-        <h3 className="text-xs dark:text-neutral-300 font-medium group-hover:text-neutral-200">
+        <h2 className="text-xs dark:text-neutral-300 font-medium group-hover:text-neutral-200">
           Certificate
           <br />
           <span className="text-neutral-400 text-xs">
             Muhammad Angga Adytya
           </span>
-        </h3>
+        </h2>
       </div>
 
       <div className="flex items-center gap-x-3 mt-6 hover:bg-neutral-800 p-2 rounded-md hover:cursor-pointer group transition-all duration-300 ease-in">
         <div className="bg-neutral-700/50 w-8 h-8 rounded-full flex items-center justify-center">
           <PiGithubLogoLight className="group-hover:text-neutral-200" />
         </div>
-        <h3 className="text-xs dark:text-neutral-300 font-medium group-hover:text-neutral-200">
+        <h2 className="text-xs dark:text-neutral-300 font-medium group-hover:text-neutral-200">
           Education
           <br />
           <span className="text-neutral-400 text-xs">
             Muhammad Angga Adytya
           </span>
-        </h3>
+        </h2>
       </div>
     </div>
   );
