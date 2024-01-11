0827a46bdaabc9058d6d2bc448c347de5a590970
Muhammad Angga Adytya <anggaanggaksp@gmail.com>
2024-01-11 12:49:47 +0700

    update: metadata layout

--- app/robots.ts
+++ app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/downloads/",
+      disallow: "/private/",
     },
     sitemap: `${domain}/sitemap.xml`,
   };
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const domain = process.env.DOMAIN || "https://angga-project.vercel.app";
+  const domain = process.env.DOMAIN || "http://localhost:3000";
   return [
     {
       url: domain,
