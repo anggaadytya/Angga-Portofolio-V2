9364f166abbe133f91c6c5a344383a6d883d9297
Muhammad Angga Adytya <anggaanggaksp@gmail.com>
2024-01-20 08:40:45 +0700

    feat : update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -7,8 +7,6 @@ export default function manifest(): MetadataRoute.Manifest {
     description: "Personal website of Angga Adytya and Showcase my projects",
     start_url: "/",
     display: "standalone",
-    background_color: "#fff",
-    theme_color: "#fff",
     icons: [
       {
         src: "/favicon.ico",
