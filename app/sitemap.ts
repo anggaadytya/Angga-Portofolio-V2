668e02eb511e8324290cff3e4eb027dc66587dda
Muhammad Angga Adytya <anggaanggaksp@gmail.com>
2025-03-10 00:08:11 +0700

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,37 +1,45 @@
+import { PROJECTS } from "@/constant/projects";
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const domain = "https://angga-project.vercel.app";
+
   return [
     {
       url: domain,
-      lastModified: new Date(),
-      changeFrequency: "yearly",
+      lastModified: new Date().toISOString(),
+      changeFrequency: "monthly",
       priority: 1,
     },
     {
       url: `${domain}/dashboard`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: `${domain}/dashboard/project`,
-      lastModified: new Date(),
+      url: `${domain}/project`,
+      lastModified: new Date().toISOString(),
       changeFrequency: "monthly",
       priority: 0.7,
     },
     {
-      url: `${domain}/dashboard/about`,
-      lastModified: new Date(),
+      url: `${domain}/about`,
+      lastModified: new Date().toISOString(),
       changeFrequency: "monthly",
       priority: 0.5,
     },
     {
-      url: `${domain}/dashboard/sharing`,
-      lastModified: new Date(),
+      url: `${domain}/sharing`,
+      lastModified: new Date().toISOString(),
       changeFrequency: "daily",
       priority: 0.3,
     },
+    ...PROJECTS.map((project) => ({
+      url: `${domain}/project/${project.slug}`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: "daily" as const,
+      priority: 1,
+    })),
   ];
 }
