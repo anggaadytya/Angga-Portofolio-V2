224616a29d3bdf9071926a593c6a6804117aef67
Muhammad Angga Adytya <anggaanggaksp@gmail.com>
2025-03-10 21:22:37 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,7 +65,7 @@ export default function RootLayout({
       <head>
         <Script
           strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=GTM-NM368W55`}
+          src={`https://www.googletagmanager.com/gtag/js?id=G-7HR300DJ9F`}
         />
         <Script
           id="google-analytics"
@@ -75,10 +75,21 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'GTM-NM368W55', { 
+              gtag('config', 'G-7HR300DJ9F', { 
                 page_path: window.location.pathname,
               });
             `,
+          }}
+        />
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NM368W55');`,
           }}
         />
         <meta
